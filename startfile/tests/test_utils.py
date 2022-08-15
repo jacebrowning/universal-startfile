db0d1e6e59b4f193ed960cd36b6f6b6c55108b03
Jace Browning <jacebrowning@gmail.com>
2022-08-15 12:08:22 -0400

    Mock startfile for Windows

--- startfile/tests/test_utils.py
+++ startfile/tests/test_utils.py
@@ -7,12 +7,13 @@ from startfile import startfile
 
 def describe_startfile():
     @patch("subprocess.call")
-    def with_file(mock_call, tmp_path, expect):
+    @patch("os.startfile", create=True)
+    def with_file(mock_startfile, mock_call, tmp_path, expect):
         path = tmp_path / "example.txt"
         path.write_text("Hello, world!")
 
         startfile(path)
-        expect(mock_call.call_count) == 1
+        expect(mock_startfile.call_count + mock_call.call_count) == 1
 
     @patch("webbrowser.open")
     def with_url(mock_open, expect):
