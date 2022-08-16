60c739f5b76c76adace3b232db34deabe5e2667f
Jace Browning <jacebrowning@gmail.com>
2022-08-16 12:07:07 -0400

    Extract tests from the package

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,9 @@
-"""Integration tests configuration file."""
+import log
 
-# pylint: disable=unused-import
 
-from startfile.tests.conftest import pytest_configure
+def pytest_configure(config):
+    """Disable verbose output when running tests."""
+    log.init(debug=True)
+
+    terminal = config.pluginmanager.getplugin("terminal")
+    terminal.TerminalReporter.showfspath = False
