f055a636493e0c064cdda7c886301d6feecf29e7
René Schwaiger <sanssecours@me.com>
2025-10-01 15:45:04 +0200

    Test: Add check for exception on non-existing file

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -15,6 +15,14 @@ def describe_startfile():
         startfile(path)
         expect(mock_startfile.call_count + mock_call.call_count) == 1
 
+    @patch("subprocess.run")
+    @patch("os.startfile", create=True)
+    def with_non_existing_file(mock_startfile, mock_call, tmp_path, expect):
+        path = tmp_path / "does not exist"
+
+        startfile(path)
+        expect(FileNotFoundError in (mock_startfile.side_effect, mock_call.side_effect))
+
     @patch("webbrowser.open")
     def with_url(mock_open, expect):
         startfile("http://example.com")
