8b6cad5ec1cb9aa474e0d80488c46957c865a878
René Schwaiger <sanssecours@me.com>
2025-10-20 10:50:57 +0200

    Library: Use different functions for Linux/macOS

--- src/startfile/utils.py
+++ src/startfile/utils.py
@@ -15,12 +15,16 @@ def startfile(value: Union[str, Path]):
 
     path = value
 
-    return startfile_posix(path) if os.name == "posix" else startfile_windows(path)
+    if platform.system() == "Darwin":
+        return startfile_macos(path)
+
+    if platform.system() == "Windows":
+        return startfile_windows(path)
+
+    return startfile_linux(path)
 
 
-def startfile_posix(path):
-    command = "open" if platform.system() == "Darwin" else "xdg-open"
-
+def startfile_command(command: str, path: str):
     try:
         return subprocess.run(
             (command, path), capture_output=True, check=True, text=True
@@ -32,6 +36,14 @@ def startfile_posix(path):
             else f"Unable to open {path}"
         )
         raise FileNotFoundError(message) from error
+
+
+def startfile_linux(path: str):
+    return startfile_command("xdg-open", path)
+
+
+def startfile_macos(path: str):
+    return startfile_command("open", path)
 
 
 def startfile_windows(path: str):
